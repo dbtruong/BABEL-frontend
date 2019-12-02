eeb0da815784cbc7844cabca44ae4aafcd212f7e
Bao Truong <duybao.truong@student.vinci.be>
2019-12-02 11:49:30 +0100

    tuto push

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          BABEL MAMENE LA TEAM FRONT END
+          BABEL app
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
