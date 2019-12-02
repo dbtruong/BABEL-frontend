f3583d7a40cdf081c999f471e88fea4eb59e91e3
Bao Truong <duybao.truong@student.vinci.be>
2019-12-02 11:42:21 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          BABEL MAMENE
+          BABEL MAMENE LA TEAM FRONT END
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
