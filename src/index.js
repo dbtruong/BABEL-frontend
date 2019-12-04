3bed10d512c83913c77c9354c07fb82ee47166a6
alexis <alexisdcl@hotmail.com>
2019-12-04 12:03:06 +0100

    little error

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import { Router, Route, Switch } from "react-router-dom";
 import './Assets/Css/index.css';
 
 import HomePage from './Components/Home/HomePage.jsx';
-import Page from './Components/NewPage/Page.jsx';
+//import Page from './Components/NewPage/Page.jsx';
 var hist = createBrowserHistory();
 
 
 ReactDOM.render(<Router history={hist}>
     <Switch>
       <Route exact path="/" component={HomePage} />
-      <Route path="/newpage" component={Page} />
+      
     </Switch>
   </Router>, document.getElementById('root'));
