34483aad250fe43aac2d5ebb45fc7705abf2075f
Bao Truong <duybao.truong@student.vinci.be>
2019-12-06 16:27:08 +0100

    added child connexion api

--- src/Components/Before_game/ChildConnexion/ChildConnexionPage.jsx
+++ src/Components/Before_game/ChildConnexion/ChildConnexionPage.jsx
@@ -2,18 +2,43 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import '../../../Assets/Css/ChildConnexionPage.css';
 import Button from 'react-bootstrap/Button';
+import ChildConnectionAPI from '../../../API/ChildConnexionAPI.js'
 
 class ChildConnexionPage extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      id : ""
+    }
+    this.api = new ChildConnectionAPI();
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(){
+    // Uncomment to link with backend
+    // this.api.getChildSession(this.state.id)
+    //     .then(res => {
+    //         console.log(res.data);
+    //     })
+  }
+
   render() {
     return (
         <nav className="container">
           <Link to={'/'}><img src="Images/logo.png" alt="logo esope" className="Logo"/></Link><br/>
           <div>
           <label>Id de l'enfant:</label><br/>
-                <input type="text" placeholder="Entrez l'id de l'enfant" className="input"></input><br/>
+                <input 
+                  type="text" 
+                  placeholder="Entrez l'id de l'enfant" 
+                  className="input"
+                  value = {this.state.id}
+                  onChange={e => this.setState({id : e.target.value})}>
+                </input><br/>
           </div>
           <br/>
-          <Link to={'/childSummary'}><Button className="button">Connexion</Button></Link>
+          <Link to={'/childSummary'}><Button className="button" onClick={this.handleClick}>Connexion</Button></Link>
         </nav>
     );
   }
