3c04d50ccb84db96d988b8fcf977375f420283a3
alexis <alexisdcl@hotmail.com>
2019-12-08 14:59:26 +0100

    SortingTopicsPage logic

---
+++ src/API/SortingTopicsAPI.js
@@ -0,0 +1,84 @@
+import API from "./API.js"
+import Axios from "axios";
+//import axios from 'axios'
+
+
+class SortingTopicsAPI extends API{
+
+    constructor(){
+        super();
+        this.endpoint = this.url + "/sortingTopics";
+    }
+
+	getImages(){
+        const listImages = [
+                [   {name : 'bus', desc : 'Prendre le bus'},
+                    {name : 'marcher', desc : 'Marcher'},
+                    {name : 'traverser', desc : 'Traverser un passage piéton'},
+                    {name : 'velo', desc : 'Faire du vélo'},
+                    {name : 'voiture', desc : 'Etre dans la voiture'}
+                ],
+                [
+                    {name : 'allumer_lumiere', desc : 'Allumer la lumière'},
+                    {name : 'balayer', desc : 'Balayer'},
+                    {name : 'machine_laver', desc : 'Utiliser la machine à laver'},
+                    {name : 'passer_piece', desc : 'Passer d\'une pièce à l\'autre'},
+                    {name : 'prothese', desc : 'Avoir une prothèse'},
+                    {name : 'ranger_espace', desc : 'Ranger son espace'},
+                    {name : 'vaisselle', desc : 'Faire la vaisselle'}
+                ],
+                [
+                    {name : 'casque_ordi', desc : 'Avoir son casque et son ordinateur'},
+                    {name : 'ecrire', desc : 'Ecrire'},
+                    {name : 'lire', desc : 'Lire'},
+                    {name : 'television', desc : 'Regarder la télévision'}
+                ],
+                [
+                    {name : 'boire_tasse', desc : 'Boire avec une tasse'},
+                    {name : 'dresser_table', desc : 'Dresser la table'},
+                    {name : 'manger_restaurant', desc : 'Manger au restaurant'},
+                    {name : 'manger_mains', desc : 'Manger avec les mains'},
+                    {name : 'preparer_manger', desc : 'Préparer à manger'},
+                    {name : 'sandwich', desc : 'Manger un sandwich'},
+                    {name : 'servir_aliments', desc : 'Servir des aliments'}
+                ],
+                [
+                    {name : 'parler_adultes', desc : 'Parler à des adultes'},
+                    {name : 'parler_communaute', desc : 'Parler en communauté'},
+                    {name : 'telephone', desc : 'Parler au téléphone'}
+                ],
+                [
+                    {name : 'acheter', desc : 'Acheter quelque chose'}
+                ],
+                [
+                    {name : 'laver_mains', desc : 'Se laver les mains'},
+                    {name : 'urgence', desc : 'Aller aux urgences'},
+                    {name : 'brosser_cheveux', desc : 'Se brosser les cheveux'},
+                    {name : 'chaussures', desc : 'Mettre ses chaussures'},
+                    {name : 'dormir', desc : 'Dormir'},
+                    {name : 'douche', desc : 'Prendre une douche'},
+                    {name : 'habits_sales', desc : 'Mettre des habits sales'},
+                    {name : 'laver_cheveux', desc : 'Se laver les cheveux'},
+                    {name : 'vetements_garderobe', desc : 'Choisir ses vêtements'},
+                    {name : 'essuyer', desc : 'S\'essuyer'},
+                    {name : 'habiller_tirette', desc : 'Mettre des habits avec une tirette'},
+                    {name : 'habiller', desc : 'S\'habiller'},
+                    {name : 'brosser_dents', desc : 'Se brosser les dents'},
+                    {name : 'lever', desc : 'Se lever'},
+                    {name : 'moucher', desc : 'Se moucher'},
+                    {name : 'secher_cheveux', desc : 'Se sécher les cheveux'},
+                    {name : 'toilettes', desc : 'Aller aux toilettes'}
+                ]
+        ]
+		return listImages;
+    }
+    
+    sendImage(name, like){
+        //axios.get()
+        let toSend = {name : name, like : like}
+        console.log(toSend);
+    }
+
+}
+
+export default SortingTopicsAPI;
--- src/Components/In_game/SortingTopicsPage/SortingTopicsPage.jsx
+++ src/Components/In_game/SortingTopicsPage/SortingTopicsPage.jsx
@@ -1,26 +1,94 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import '../../../Assets/Css/SortingTopicsPage.css';
+import SortingTopicsAPI from '../../../API/SortingTopicsAPI.js'
 
 
 class SortingTopicsPage extends Component {
-  render() {
+  
+    constructor(){
+        super();
+        this.state = {
+			listImages : [],
+			currentImg : "",
+			currentDesc : "",
+			indexCat : 0,
+            indexImg : 0
+        }
+		/*this.state = {
+			listImages : [],
+			currentImg : "",
+			currentDesc : "",
+			indexCat : 0,
+            indexImg : 0,
+            cmpt1 : 0,
+            cmpt2 : 0, 
+            cmpt3 : 0, 
+            listImagesPresent :[],
+            //tableau un peu inutile
+            listImagesNotPresent :[],
+            listImagesWanted :[],
+		}*/
+		this.api = new SortingTopicsAPI();
+		this.state.listImages = this.api.getImages();
+		this.state.currentImg = "Images/deplacements/" + this.state.listImages[this.state.indexCat][this.state.indexImg].name + ".jpg";
+		this.state.currentDesc = this.state.listImages[this.state.indexCat][this.state.indexImg].desc;
+		this.handleClick = this.handleClick.bind(this);
+	}
+   
+    handleClick(e){
+		this.api.sendImage(this.state.listImages[this.state.indexCat][this.state.indexImg].name, e.target.name);
+		const catName = ["deplacements", "habitation", "loisirs", "nutrition", "relationscom", "responsabilites", "soinspersonnels"]
+		const nbImages = [5,7,4,7,3,1,17]
+		let tempImg = this.state.indexImg +1 ;  
+		let tempCat = this.state.indexCat +1 ; 
+		if (tempImg >= nbImages[this.state.indexCat]){
+			this.state.indexCat = tempCat;
+			tempImg = 0;
+			this.state.indexImg = tempImg;
+        }
+        // tableaux contenant les choix
+        /*if(e.target.name ==="1"){
+            this.state.listImagesPresent[this.state.cmpt1]= this.state.listImages[this.state.indexCat][this.state.indexImg].name; 
+            this.state.cmpt1 = this.state.cmpt1 +1; 
+        }else if (e.target.name ==="2"){
+            this.state.listImagesNotPresent[this.state.cmpt2]= this.state.listImages[this.state.indexCat][this.state.indexImg].name; 
+            this.state.cmpt2 = this.state.cmpt2 +1; 
+        }else {
+            this.state.listImagesWanted[this.state.cmpt3]= this.state.listImages[this.state.indexCat][this.state.indexImg].name; 
+            this.state.cmpt3 = this.state.cmpt3 +1; 
+        }*/
+
+        this.state.indexImg = tempImg;
+        
+		this.setState({
+			currentImg : 
+			"Images/" + catName[this.state.indexCat] + "/" + this.state.listImages[this.state.indexCat][this.state.indexImg].name + ".jpg",
+			currentDesc :
+			this.state.listImages[this.state.indexCat][this.state.indexImg].desc
+        })   
+    }
+
+    
+  
+    render() {
     return (
         <nav class="container">
             <div>
             
-            <Link to={'/'}><img src="Images/logo.png" alt="logo habitudes" className="imgHabit"/></Link><br/>
+            <p>{this.state.currentDesc}</p>
+				<img src={this.state.currentImg} alt={this.state.currentDesc} className="imgHabit"/>
             </div>
             <br/>
             <h4>Selectionnez l'un des trois choix selon vos habitudes</h4>
-            <br/><br/><br/><br/><br/>
+            <br/><br/><br/>
             <div>
-                <table>
+                <table class="table_order">
                     <tbody>
                         <tr>
-                            <td class="cel"><button class="Sorted_button"><Link to={'/choiceLike'} className="nav-link">Présent dans mes habitudes</Link></button></td>
-                            <td class="cel"><button class="Sorted_button"><Link to={'/choiceLike'} className="nav-link">Non-présent dans mes habitudes</Link></button></td>
-                            <td class="cel"><button class="Sorted_button"><Link to={'/choiceLike'} className="nav-link">Je veux dans mes habitudes</Link></button></td>
+                            <td class="cel"><button name="1" class="Sorted_button" onClick={this.handleClick}>Présent dans mes habitudes</button></td>
+                            <td class="cel"><button name="2" class="Sorted_button" onClick={this.handleClick}>Non-présent dans mes habitudes</button></td>
+                            <td class="cel"><button name="3" class="Sorted_button" onClick={this.handleClick}>Je veux dans mes habitudes</button></td>
                         </tr>
                     </tbody>
                 </table>
