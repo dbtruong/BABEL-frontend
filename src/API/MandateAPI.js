5dc4e8f26c6e1ca9e941043ce39f16c545231636
alexis <alexisdcl@hotmail.com>
2019-12-10 14:40:31 +0100

    changes

--- src/API/MandateAPI.js
+++ src/API/MandateAPI.js
@@ -2,8 +2,15 @@ import axios from "axios";
 
 class MandateAPI{
 
-    SendMandate(childId,instigator,comments,date){
-       axios.post('/api/v1/');
+    sendMandate(childId,instigator,demand,date){
+       axios.get('/api/v1/children',{
+        mandate:{
+            insert_date : date,
+            id_child : childId,
+            instigator : instigator,
+            demand : demand
+        } 
+       }).then(res => console.log(res))
     }
 
 }
