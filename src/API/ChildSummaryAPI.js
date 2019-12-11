4d88ecfce98da935fc05faa506b7a6151155d756
Bao Truong <duybao.truong@student.vinci.be>
2019-12-11 09:10:16 +0100

    added childSummary for children

--- src/API/ChildSummaryAPI.js
+++ src/API/ChildSummaryAPI.js
@@ -8,7 +8,7 @@ class ChildSummaryAPI{
         .then(res => response = res.data)
         .catch(error => console.log(error));
 
-        console.log(response)
+        //Put every images in an array and send objects for the summary
         let array = [];
         for(let i = 0; i < response.length; i++){
             let tempArray = [];
@@ -25,19 +25,40 @@ class ChildSummaryAPI{
                 images : tempArray
             })
         }
-        console.log(array);
 
         //response doit être un json qui contient images et un attribut date
         let summary = [
             {   date : "3 décembre 2019",
                 images : [
-                    { name : "bus", like : true, help : true, happy : false},
-                    { name : "marcher", like : true, help : false, happy : false},
-                    { name : "velo", like : false, help : true, happy : false}
+                    { name : "/deplacements/bus", like : true, help : true, happy : false},
+                    { name : "/deplacements/marcher", like : true, help : false, happy : false},
+                    { name : "/habitation/vaisselle", like : false, help : true, happy : true},
+                    { name : "/loisirs/casque_ordi", like : true, help : false, happy : false},
+                    { name : "/loisirs/television", like : true, help : false, happy : true},
+                    { name : "/soinspersonnels/brosser_dents", like : false, help : true, happy : true}
+                ]
+            },
+            {   date : "15 novembre 2019",
+                images : [
+                    { name : "/deplacements/traverser", like : true, help : false, happy : false},
+                    { name : "/deplacements/voiture", like : true, help : false, happy : false},
+                    { name : "/habitation/balayer", like : true, help : true, happy : true},
+                    { name : "/loisirs/ecrire", like : false, help : true, happy : false},
+                    { name : "/nutrition/boire_tasse", like : true, help : false, happy : true},
+                    { name : "/nutrition/sandwich", like : false, help : false, happy : false}
+                ]
+            },
+            {   date : "12 septembre 2019",
+                images : [
+                    { name : "/deplacements/velo", like : false, help : true, happy : false},
+                    { name : "/habitation/ranger_espace", like : true, help : true, happy : false},
+                    { name : "/relationscom/parler_adultes", like : false, help : true, happy : false},
+                    { name : "/loisirs/casque_ordi", like : true, help : false, happy : false},
+                    { name : "/soinspersonnels/dormir", like : false, help : false, happy : true},
+                    { name : "/soinspersonnels/toilettes", like : true, help : false, happy : true}
                 ]
             }
         ]
-        console.log("ideal : ", summary)
         return summary;
     }
 }
--- src/Components/Before_game/ChildSummary/ChildSummary.jsx
+++ src/Components/Before_game/ChildSummary/ChildSummary.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
+import Button from 'react-bootstrap/Button';
 
 import '../../../Assets/Css/ChildSummary.css';
 import ChildSummaryAPI from '../../../API/ChildSummaryAPI.js'
@@ -11,48 +12,70 @@ class ChildSummary extends Component{
         this.state = {
             listImages : [],
             date : "",
-            summary : []
+            summary : [],
+            index : 0,
+            maxIndex : 0
         }
         this.api = new ChildSummaryAPI();
         this.createSummary = this.createSummary.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
 
     componentDidMount(){
-        this.createSummary();
+        this.createSummary(this.state.index);
+    }
+
+    handleClick(e){
+        if (e.target.alt == "suivant"){
+            if(this.state.index < this.state.maxIndex){
+                this.state.index += 1;
+            }
+        } else {
+            if(this.state.index >= 1){
+                this.state.index -= 1;
+            }
+        }
+        this.createSummary(this.state.index);
     }
 
     //Il faudra gérer les différentes catégories en fonction de l'image
-    async createSummary(){
+    async createSummary(index){
         //Can't seem to use setState somehow
         //this.setState({listImages : this.api.getChildSummary()});
         let sum = await this.api.getChildSummary();
+        this.setState({maxIndex : sum.length-1})
         //console.log("resp : ", sum)
         //console.log(sum[0].date)
-        this.state.listImages = sum[0].images;
-        this.state.date = sum[0].date;
+        this.state.listImages = sum[index].images;
+        this.state.date = sum[index].date;
         //console.log(this.state.listImages);
         let summary = [];
         let list = this.state.listImages;
         for(let i = 0; i < list.length; i++){
             let children = []
             let desc = list[i].name;
-            let path = "Images/deplacements/"+desc+".jpg";
-            
+            let path = "Images/"+desc+".jpg";
+
+            if (!summary.length){
+                summary.push(<img className="navigation" src="/Images/previous.png" alt="précédent" onClick={this.handleClick}/>)
+                summary.push(<a className="date">Séance du {this.state.date}</a>)
+                summary.push(<img className="navigation" src="/Images/next.png" alt="suivant" onClick={this.handleClick}/>)
+            }
             children.push(<img src={path} alt={desc}/>)
             if (list[i].like){
-                children.push(<img src="/Images/like.png" alt="j'aime"/>)
+                children.push(<img className="likes" src="/Images/like.png" alt="j'aime"/>)
             } else {
-                children.push(<img src="/Images/dislike.png" alt="je n'aime pas"/>)
+                children.push(<img className="likes" src="/Images/dislike.png" alt="je n'aime pas"/>)
             }
             if (list[i].help){
-                children.push(<img src="/Images/help.png" alt="j'ai besoin d'aide"/>)
+                children.push(<img className="help" src="/Images/help.png" alt="j'ai besoin d'aide"/>)
             } else {
-                children.push(<img src="/Images/nohelp.png" alt="je n'ai pas besoin d'aide"/>)
+                children.push(<img className="help" src="/Images/nohelp.png" alt="je n'ai pas besoin d'aide"/>)
             }
             if (list[i].happy){
-                children.push(<img src="/Images/happy.png" alt="je suis content"/>)
+                children.push(<img className="happy" src="/Images/happy.png" alt="je suis content"/>)
             } else {
-                children.push(<img src="/Images/unhappy.png" alt="je ne suis pas content"/>)
+                children.push(<img className="happy" src="/Images/unhappy.png" alt="je ne suis pas content"/>)
             }
             summary.push(<div>{children}</div>)
         }
@@ -62,12 +85,15 @@ class ChildSummary extends Component{
     
 
     render(){
+        let button = <Link to={"/"}><Button className="button">Retour</Button></Link>;
+        if (localStorage.getItem('role') != "parent"){
+            button = <Link to={"/start"}><Button className="button">Page suivante</Button></Link>
+        }
         return(
             <div className="container">
-                <h1>Synthèse de l'enfant</h1><br/>
+                <h1>Synthèse de l'enfant</h1><br/><br/>
                 {this.state.summary}<br/>
-                <h2>Séance du  {this.state.date}</h2>
-                <Link to={"/start"}><img src="Images/next.png"/></Link>
+                {button}
             </div>
         )
     }
